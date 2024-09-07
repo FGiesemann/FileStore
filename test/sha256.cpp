e96f419d8bb7289e14a537edc3a82920585d523b
Florian Giesemann <florian.giesemann@gmail.com>
2024-09-07 16:51:47 +0200

    Add edge test case

--- test/sha256.cpp
+++ test/sha256.cpp
@@ -18,4 +18,9 @@ TEST_CASE("sha256 algorithm", "[hash]") {
     std::string data{
         "adsflbhroliswuh seiguhlsi glgikbsrgoiudfhgsgb<so sjln3408sefs974p94bhjs c jswfos84zsbu hsklusughdsov8vs e iusebixdhlbslisuegp8qw73bv6z 38w g 3wp08w7gfasjuhvas f8 3fvpoa8wzauvfwfa lo87 asg"};
     REQUIRE(hash(data) == "ec4f8460cdc835d41add660236f5059f14e0bd35031df85c6da72bf753ae4efb");
+
+    // Data length == SHA256 buffer size (64 bytes)
+    data = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl";
+    REQUIRE(data.length() == 64);
+    REQUIRE(hash(data) == "2fcd5a0d60e4c941381fcc4e00a4bf8be422c3ddfafb93c809e8d1e2bfffae8e");
 }
